fc6fea81c3391c026f17871774b02dd489f0dfb9
김웅휘 <woongkaa@gim-unghwiui-MacBook-Air.local>
2015-05-16 16:33:39 +0900

    post를 변경해야했는데, Category를 변경함.

--- sample_board/admin.py
+++ sample_board/admin.py
@@ -3,13 +3,13 @@ from sample_board.models import Category, Post
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'category', 'title', 'created']
+    list_display = ['id', 'title', 'created']
     list_editable = ['name']
     search_fields = ['name']
     ordering = ['name']
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created']
+    list_display = ['id', 'category', 'title', 'created']
     list_filter = ['category']
     list_display_links = ['id', 'title']
     search_fields = ['title', 'content', ]

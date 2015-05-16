f46e2f4cf2d8c18a5ab58e3a4bc2dc7795ebe5e8
김웅휘 <woongkaa@gim-unghwiui-MacBook-Air.local>
2015-05-16 16:04:21 +0900

    모델링 및 admin 설

---
+++ sample_board/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from sample_board.models import Category, Post
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_editable = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created']
+    list_filter = ['category']
+    list_display_links = ['id', 'title']
+    search_fields = ['title', 'content', ]
+    ordering = ['created']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- tutoDjangoBoard/settings.py
+++ tutoDjangoBoard/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sample_board',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'tutoDjangoBoard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tutoBoard',
+        'USER': 'woongkaa',
+        'PASSWORD': '12345678',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
